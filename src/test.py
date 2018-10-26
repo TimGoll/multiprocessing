cc4ca5d27adfdbd3f93ff4a7f27dd7d034d8984a
Tim Goll <tim@timgoll.de>
2018-10-26 18:50:27 +0200

    cleaned up code

---
+++ src/test.py
@@ -0,0 +1,61 @@
+from time import sleep
+
+import QueueHandler, MultiprocessingBase
+
+################ USER CLASSES ################
+
+class ClassReader(MultiprocessingBase.MultiprocessingBase):
+    def __init__(self, queue_handler, main_queue_name):
+        super(ClassReader, self).__init__(queue_handler)
+        self.main_queue_name = main_queue_name
+        self.flag = 'REDR'
+
+    def run_process(self):
+        #super(ClassReader, self).run_process()
+        data = self.queue_handler.get(self.main_queue_name)
+        if (data != None):
+            print('read data: ' + str(data))
+        sleep(0.1)
+
+class ClassInserter(MultiprocessingBase.MultiprocessingBase):
+    def __init__(self, queue_handler, main_queue_name):
+        super(ClassInserter, self).__init__(queue_handler)
+        self.main_queue_name = main_queue_name
+        self.flag = 'INST'
+
+        self.counter = 0
+
+    def run_process(self):
+        self.queue_handler.put(self.main_queue_name, self.counter)
+        self.counter += 1
+        sleep(0.251)
+
+
+
+		
+def main():
+	print "main"
+	try:
+		queueHandler = QueueHandler.QueueHandler()
+		queueHandler.initQueue('main_queue')
+
+		classReader   = ClassReader(queue_handler = queueHandler, main_queue_name = 'main_queue')
+		#create another instance to check the uniqueness of the queue name (address)
+		classReader2  = ClassReader(queue_handler = queueHandler, main_queue_name = 'main_queue')
+		classInserter = ClassInserter(queue_handler = queueHandler, main_queue_name = 'main_queue')
+
+		classReader.start_process()
+		classReader2.start_process()
+		classInserter.start_process()
+
+		while (True): #keep main process alive
+			sleep(1)
+			
+	except KeyboardInterrupt:
+		classReader.stop_process()
+		classReader2.stop_process()
+		classInserter.stop_process()
+
+if __name__ == "__main__":
+	print ("name is main")
+	main()
